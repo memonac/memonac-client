d484caf5ef805b30c7098ede3391b5c773ffd65f
woojeong kong <coolstevevc@gmail.com>
2022-02-06 05:00:22 +0900

    feat: add main reducer

--- src/app/configureStrore.js
+++ src/app/configureStrore.js
@@ -4,10 +4,12 @@ import { all } from "redux-saga/effects";
 import logger from "redux-logger";
 
 import authReducer from "../features/auth/authSlice";
+import mainReducer from "../features/main/mainSlice";
 import { userSaga } from "../features/auth/authSaga";
 
 const reducer = combineReducers({
   auth: authReducer,
+  main: mainReducer,
 });
 
 const sagaMiddleware = createSagaMiddleware();
