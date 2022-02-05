510c46a964a88d0f1be01137f9804f773d255a36
woojeong kong <coolstevevc@gmail.com>
2022-02-06 05:00:42 +0900

    feat: add main component

--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+import Tag from "../../components/Tag";
+import Nav from "../../components/Nav";
+
+import { getMemoRoomListRequest } from "./mainSlice";
+import { logoutRequest } from "../auth/authSlice";
 
 function Main() {
-  return <h1>This is memoRoom Main</h1>;
+  const displayedTags = useSelector((state) => state.main.displayedTags);
+  const tagInfo = useSelector((state) => state.main.tagInfo);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMemoRoomListRequest());
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Sidebar >
+        {displayedTags.map((value, key) => {
+          return (
+            <Tag
+              key={value}
+              text={`${value}`}
+              isSelected={tagInfo[value].isSelected}
+            />
+          );
+        })}
+      </Sidebar>
+    </>
+  );
 }
 
 export default Main;
