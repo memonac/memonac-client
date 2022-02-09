8900608fdb77bebb6cf17392650539d42269e625
woojeong kong <coolstevevc@gmail.com>
2022-02-10 04:48:47 +0900

    feat: add memoSaga in redux-saga

--- src/app/configureStrore.js
+++ src/app/configureStrore.js
@@ -8,6 +8,7 @@ import mainReducer from "../features/main/mainSlice";
 import memoRoomReducer from "../features/memoroom/memoRoomSlice";
 import { userSaga } from "../features/auth/authSaga";
 import { memoListSaga } from "../features/main/mainSaga";
+import { memoRoomSaga } from "../features/memoroom/memoRoomSaga";
 import { socketSagas } from "../features/memoroom/socketSaga";
 
 const reducer = combineReducers({
@@ -19,7 +20,7 @@ const reducer = combineReducers({
 const sagaMiddleware = createSagaMiddleware();
 
 function* rootSaga() {
-  yield all([userSaga(), memoListSaga(), socketSagas()]);
+  yield all([userSaga(), memoListSaga(), memoRoomSaga(), socketSagas()]);
 }
 
 const createStore = () => {
