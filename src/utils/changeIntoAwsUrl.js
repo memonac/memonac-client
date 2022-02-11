6d702ea331c076e7121ef63b6386d1dc43559b65
okcorn <jsok79now@gmail.com>
2022-02-12 03:35:23 +0900

    chore: remove unnecessary file

--- src/utils/changeIntoAwsUrl.js
+++
@@ -1,5 +0,0 @@
-function changeIntoAwsUrl(file) {
-  return `https://${process.env.REACT_APP_AWS_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_BUCKET_REGION}.amazonaws.com/${file.key}`;
-}
-
-export default changeIntoAwsUrl;
