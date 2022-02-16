15619b89ee29d33c3a8ce9482dbeeee367972552
leesuin <suin0212@gmail.com>
2022-02-16 17:46:42 +0900

    style: index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import createStore from "./app/configureStrore";
 import App from "./app/App";
